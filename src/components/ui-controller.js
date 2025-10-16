72836fa9daa53d27e45c39f785a7b2cce36f015c
Miroslavv99 <mbochko99@gmail.com>
2025-10-16 10:45:10 +0300

    refactor: split handleCellClick into separate methods

--- src/components/ui-controller.js
+++ src/components/ui-controller.js
@@ -1,14 +1,24 @@
 export class UIController {
   constructor(gameController, renderer) {
-    this.mode = "placement"; // Current mode: "placement" or "attack"
+    this.mode = "placement";
     this.selectedShip = null;
+    this.isHorizontal = true;
+    this.orientationButton = document.querySelector(".orientation");
     this.gameBoardContainer = document.querySelector(".game-board");
     this.playerOneCells = document.querySelector(".grid-one");
     this.playerTwoCells = document.querySelector(".grid-two");
     this.shipBase = document.querySelector(".my-ships");
-    this.playerTwoShips = document.querySelector(".opponent-ships");
     this.gameController = gameController;
     this.renderer = renderer;
+    this.initOrientationButton();
+  }
+
+  initOrientationButton() {
+    this.orientationButton.addEventListener("click", () => {
+      this.isHorizontal = !this.isHorizontal;
+      this.renderer.updateOrientationButton(this.isHorizontal);
+      console.log(this.isHorizontal);
+    });
   }
 
   shipSelector() {
@@ -39,112 +49,147 @@ export class UIController {
       if (!cell) return;
       let owner = event.target.dataset.owner;
 
-      if (this.mode === "placement") {
-        if (!this.gameController.playerOne.gameBoard.allShipsPlaced) {
-          if (!this.selectedShip) return;
-          if (
-            this.gameController.playerOne.gameBoard.placedShips.has(
-              this.selectedShip.id
-            )
-          )
-            return;
-          if (owner === "p1") {
-            this.gameController.playerOne.gameBoard.placeShip(
-              Number(this.selectedShip.length),
-              this.selectedShip.id,
-              cell
-            );
-          }
-
-          if (this.gameController.playerOne.gameBoard.allShipsPlaced) {
-            this.renderer.renderBattleInfo(
-              `Player ${this.gameController.playerTwo.name} Put Your Ships`
-            ); // Prompt next player
-            this.renderer.showPlayerTwoBoard(); // Show Player Two's board
-            this.renderer.hidePlayerOneBoard(); // Hide Player One's board
-          }
-
-          if (this.selectedShip) this.selectedShip = null;
-        }
-
-        if (this.gameController.playerOne.gameBoard.allShipsPlaced) {
-          if (!this.selectedShip) return;
-          if (
-            this.gameController.playerTwo.gameBoard.placedShips.has(
-              this.selectedShip.id
-            )
-          )
-            return;
-          if (owner === "p2") {
-            this.gameController.playerTwo.gameBoard.placeShip(
-              Number(this.selectedShip.length),
-              this.selectedShip.id,
-              cell
-            );
-
-            if (this.selectedShip) this.selectedShip = null;
-
-            if (this.gameController.playerTwo.gameBoard.allShipsPlaced) {
-              this.mode = "attack"; // Switch to attack mode after all ships placed
-              return;
-            }
-          }
-        }
-      }
-
-      if (
-        this.mode === "attack" &&
-        this.gameController.currentPlayer === this.gameController.playerTwo &&
-        owner === "p1" &&
-        !this.gameController.checkWinner()
-      ) {
-        let attackInfo = this.gameController.currentPlayer.attack(cell); // Perform attack
-        if (attackInfo) {
-          this.renderer.markCell(`p1-${cell}`, "hit"); // Mark hit
-          this.renderer.renderBattleInfo(
-            `Hit! Player ${this.gameController.playerTwo.name} attack again!`
-          );
-        } else {
-          if (this.gameController.checkWinner()) {
-            this.renderer.markCell(`p1-${cell}`, "hit"); // Last hit during victory
-            this.renderer.renderBattleInfo(
-              `Player ${this.gameController.currentPlayer.name} Win!`
-            );
-          } else {
-            this.renderer.markCell(`p1-${cell}`, "miss"); // Mark miss
-            this.renderer.renderBattleInfo(
-              `Miss! Player ${this.gameController.playerOne.name} attack!`
-            );
-          }
-        }
-      }
-
-      if (
-        this.mode === "attack" &&
-        this.gameController.currentPlayer === this.gameController.playerOne &&
-        owner === "p2" &&
-        !this.gameController.checkWinner()
-      ) {
-        let attackInfo = this.gameController.currentPlayer.attack(cell);
-        if (attackInfo) {
-          this.renderer.markCell(`p2-${cell}`, "hit");
-          this.renderer.renderBattleInfo(
-            `Hit! Player ${this.gameController.playerOne.name} attack again!`
-          );
-        } else {
-          if (this.gameController.checkWinner()) {
-            this.renderer.markCell(`p2-${cell}`, "hit");
-            this.renderer.renderBattleInfo(
-              `Player ${this.gameController.currentPlayer.name} Win!`
-            );
-          } else {
-            this.renderer.markCell(`p2-${cell}`, "miss");
-            this.renderer.renderBattleInfo(
-              `Miss! Player ${this.gameController.playerTwo.name} attack!`
-            );
-          }
-        }
+      if (this.mode === "attack") {
+        this.handleAttack(cell, owner);
+      } else {
+        this.handlePlacement(cell, owner);
       }
     });
+  }
+
+  handleAttack(cell, owner) {
+    if (
+      this.gameController.currentPlayer === this.gameController.playerTwo &&
+      owner === "p1" &&
+      !this.gameController.checkWinner()
+    ) {
+      let attackInfo = this.gameController.currentPlayer.attack(cell); // Perform attack
+      if (attackInfo) {
+        this.renderer.markCell(`p1-${cell}`, "hit"); // Mark hit
+        this.renderer.renderBattleInfo(
+          `Hit! Player ${this.gameController.playerTwo.name} attack again!`
+        );
+      } else {
+        if (this.gameController.checkWinner()) {
+          this.renderer.markCell(`p1-${cell}`, "hit"); // Last hit during victory
+          this.renderer.renderBattleInfo(
+            `Player ${this.gameController.currentPlayer.name} Win!`
+          );
+        } else {
+          this.renderer.markCell(`p1-${cell}`, "miss"); // Mark miss
+          this.renderer.renderBattleInfo(
+            `Miss! Player ${this.gameController.playerOne.name} attack!`
+          );
+        }
+      }
+    }
+
+    if (
+      this.gameController.currentPlayer === this.gameController.playerOne &&
+      owner === "p2" &&
+      !this.gameController.checkWinner()
+    ) {
+      let attackInfo = this.gameController.currentPlayer.attack(cell);
+      if (attackInfo) {
+        this.renderer.markCell(`p2-${cell}`, "hit");
+        this.renderer.renderBattleInfo(
+          `Hit! Player ${this.gameController.playerOne.name} attack again!`
+        );
+      } else {
+        if (this.gameController.checkWinner()) {
+          this.renderer.markCell(`p2-${cell}`, "hit");
+          this.renderer.renderBattleInfo(
+            `Player ${this.gameController.currentPlayer.name} Win!`
+          );
+        } else {
+          this.renderer.markCell(`p2-${cell}`, "miss");
+          this.renderer.renderBattleInfo(
+            `Miss! Player ${this.gameController.playerTwo.name} attack!`
+          );
+        }
+      }
+    }
+  }
+
+  handlePlacement(cell, owner) {
+    // Ship placement for player one
+    if (!this.gameController.playerOne.gameBoard.allShipsPlaced) {
+      if (!this.selectedShip) return;
+      if (
+        this.gameController.playerOne.gameBoard.placedShips.has(
+          this.selectedShip.id
+        )
+      ) {
+        return;
+      }
+
+      if (owner === "p1") {
+        this.gameController.playerOne.gameBoard.placeShip(
+          this.isHorizontal,
+          Number(this.selectedShip.length),
+          this.selectedShip.id,
+          cell
+        );
+
+        this.renderer.markShipCells(
+          cell,
+          Number(this.selectedShip.length),
+          this.isHorizontal,
+          "p1"
+        );
+
+        console.log(this.gameController.playerOne.gameBoard.ships);
+        console.log(this.gameController.playerOne.gameBoard.board);
+      }
+
+      if (this.gameController.playerOne.gameBoard.allShipsPlaced) {
+        this.renderer.renderBattleInfo(
+          `Player ${this.gameController.playerTwo.name} Put Your Ships`
+        );
+        this.renderer.showPlayerTwoBoard();
+        this.renderer.hidePlayerOneBoard();
+      }
+
+      if (this.selectedShip) this.selectedShip = null;
+    }
+
+    // Ship placement for player Two
+    if (this.gameController.playerOne.gameBoard.allShipsPlaced) {
+      if (!this.selectedShip) return;
+      if (
+        this.gameController.playerTwo.gameBoard.placedShips.has(
+          this.selectedShip.id
+        )
+      )
+        return;
+
+      if (owner === "p2") {
+        this.gameController.playerTwo.gameBoard.placeShip(
+          this.isHorizontal,
+          Number(this.selectedShip.length),
+          this.selectedShip.id,
+          cell
+        );
+
+        this.renderer.markShipCells(
+          cell,
+          Number(this.selectedShip.length),
+          this.isHorizontal,
+          "p2"
+        );
+        console.log(this.gameController.playerTwo.gameBoard.board);
+
+        if (this.selectedShip) this.selectedShip = null;
+
+        if (this.gameController.playerTwo.gameBoard.allShipsPlaced) {
+          this.renderer.clearCells();
+          this.renderer.renderBattleInfo(
+            `Player ${this.gameController.currentPlayer.name} attack!`
+          );
+          this.mode = "attack";
+          return;
+        }
+      }
+    }
   }
 }
