1667390c9542a90de0705ed11428238909876297
Miroslavv99 <mbochko99@gmail.com>
2025-10-22 11:16:26 +0300

    feat: listener for the play against bot button has been implemented

--- src/index.js
+++ src/index.js
@@ -1,35 +1,41 @@
 import "./styles/styles.css";
 
-import { Renderer } from "./components/renderer.js";
+import { GameUiRenderer } from "./components/renderer.js";
 import { GameController } from "./components/game-controller.js";
 import { UIController } from "./components/ui-controller.js";
 import { FormHandler } from "./components/form-handler.js";
 
 const startButton = document.querySelector(".start");
-const menu = document.querySelector(".menu");
+const menuContainer = document.querySelector(".menu");
 const playerForm = document.querySelector(".player-form");
 const botButton = document.querySelector(".bot");
 const playerButton = document.querySelector(".player");
 
-const renderer = new Renderer();
-const gameController = new GameController(renderer);
-
 document.addEventListener("DOMContentLoaded", () => {
-  renderer.renderGrid();
-  renderer.renderOponentGrid();
-  renderer.showPlayerOneBoard();
-  renderer.renderBattleInfo("Wellcome to the Battleship game!");
-
-  const uiController = new UIController(gameController, renderer);
+  const gameUiRenderer = new GameUiRenderer();
+  const gameController = new GameController(gameUiRenderer);
+  const uiController = new UIController(gameController, gameUiRenderer);
   const formHandler = new FormHandler(gameController, uiController);
 
+  gameUiRenderer.renderGrid();
+  gameUiRenderer.renderOponentGrid();
+  gameUiRenderer.showPlayerOneBoard();
+  gameUiRenderer.renderBattleInfo("Wellcome to the Battleship game!");
+
   startButton.addEventListener("click", () => {
-    menu.classList.toggle("showing");
+    menuContainer.classList.toggle("showing");
     startButton.disabled = true;
   });
 
   playerButton.addEventListener("click", () => {
     playerForm.classList.toggle("showing");
     formHandler.playerFormInit();
+  });
+
+  botButton.addEventListener("click", () => {
+    gameController.isBotMode = true;
+    menuContainer.classList.toggle("showing");
+    uiController.shipSelector();
+    uiController.handleCellClick();
   });
 });
