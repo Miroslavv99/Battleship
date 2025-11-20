b43275dc1cac606150ad1a6b7f49515c541b5d4c
Miroslavv99 <mbochko99@gmail.com>
2025-11-20 22:43:10 +0200

    update webpack.prod

--- webpack.prod.cjs
+++ webpack.prod.cjs
@@ -2,9 +2,27 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.cjs");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require("path");
 
 module.exports = merge(common, {
   mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+    assetModuleFilename: "assets/images/[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|webp|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[name][ext]",
+        },
+      },
+    ],
+  },
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
